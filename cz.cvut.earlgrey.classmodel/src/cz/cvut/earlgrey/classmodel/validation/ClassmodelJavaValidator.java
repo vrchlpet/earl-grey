5e20691c0b657bded14373515f8968a19938046f
mazanma3 <mazanma3@fel.cvut.cz>
2011-12-11 03:42:11 +0100

    Added OCL support (fix #5)

--- cz.cvut.earlgrey.classmodel/src/cz/cvut/earlgrey/classmodel/validation/ClassmodelJavaValidator.java
+++ cz.cvut.earlgrey.classmodel/src/cz/cvut/earlgrey/classmodel/validation/ClassmodelJavaValidator.java
@@ -1,11 +1,16 @@
 package cz.cvut.earlgrey.classmodel.validation;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.ocl.examples.xtext.completeocl.validation.CompleteOCLEObjectValidator;
 import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.validation.EValidatorRegistrar;
 
 import cz.cvut.earlgrey.classmodel.classmodel.ClassmodelPackage;
 import cz.cvut.earlgrey.classmodel.classmodel.Import;
 
 public class ClassmodelJavaValidator extends AbstractClassmodelJavaValidator {
+
+	private static final String OCL_PATH = "/cz.cvut.earlgrey.classmodel.ocl/model/Classmodel.ocl";
 
 	/**
 	 * Checks the import URI is not null or empty.
@@ -20,6 +25,19 @@ public class ClassmodelJavaValidator extends AbstractClassmodelJavaValidator {
 			error("Imported resource could not be found.",
 					ClassmodelPackage.Literals.IMPORT__IMPORT_URI);
 			return;
+		}
+	}
+
+	@Override
+	public void register(EValidatorRegistrar registrar) {
+		super.register(registrar);
+		ClassmodelPackage ePackage = ClassmodelPackage.eINSTANCE;
+		try {
+			URI oclURI = URI.createPlatformResourceURI(OCL_PATH, true);
+			registrar.register(ePackage, new CompleteOCLEObjectValidator(
+					ePackage, oclURI));
+		} catch (Exception e) {
+			System.out.println("error");
 		}
 	}
 
