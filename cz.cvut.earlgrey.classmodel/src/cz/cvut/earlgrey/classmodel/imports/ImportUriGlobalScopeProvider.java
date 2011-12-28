cd85d29e209acc64c7c0cc52b94081f20eeb9b8d
mazanma3 <mazanma3@fel.cvut.cz>
2011-12-28 03:02:45 +0100

    Import URI

--- cz.cvut.earlgrey.classmodel/src/cz/cvut/earlgrey/classmodel/imports/ImportUriGlobalScopeProvider.java
+++ cz.cvut.earlgrey.classmodel/src/cz/cvut/earlgrey/classmodel/imports/ImportUriGlobalScopeProvider.java
@@ -4,8 +4,11 @@
 package cz.cvut.earlgrey.classmodel.imports;
 
 import java.util.LinkedHashSet;
+import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import cz.cvut.earlgrey.xtext.scoping.ImportUriGlobalScope;
 
 /**
@@ -14,12 +17,43 @@ import cz.cvut.earlgrey.xtext.scoping.ImportUriGlobalScope;
 public class ImportUriGlobalScopeProvider extends ImportUriGlobalScope {
 
 	public static final String DATA_TYPES = "/cz.cvut.earlgrey.classmodel/model/primitiveDataTypes.cm";
+	private static Logger log = Logger
+			.getLogger(ImportUriGlobalScopeProvider.class);
 
 	@Override
 	protected LinkedHashSet<URI> getImportedUris(final Resource resource) {
 		LinkedHashSet<URI> temp = super.getImportedUris(resource);
-		temp.add(URI.createPlatformResourceURI(DATA_TYPES, true));
+		URI plugin = URI.createPlatformPluginURI(DATA_TYPES, true);
+		if (!validURI(plugin)) {
+			URI local = URI.createPlatformResourceURI(DATA_TYPES, true);
+			if (validURI(local)) {
+				System.out.println("lol?");
+				temp.add(local);
+			}
+			return temp;
+		}
+		temp.add(plugin);
 		return temp;
+	}
+
+	/**
+	 * Validates given URI which contains primitiveDataTypes.cm.
+	 * 
+	 * @param uri
+	 * @return
+	 */
+	private boolean validURI(URI uri) {
+		ResourceSet set = new ResourceSetImpl();
+		try {
+			Resource resource = set.getResource(uri, true);
+			if (resource == null) {
+				return false;
+			}
+		} catch (Exception ex) {
+			log.error("Could not load primitiveDataTypes with URI " + uri);
+			return false;
+		}
+		return true;
 	}
 
 }
