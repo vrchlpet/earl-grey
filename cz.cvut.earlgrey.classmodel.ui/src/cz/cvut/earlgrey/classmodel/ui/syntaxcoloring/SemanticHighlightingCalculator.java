3819bebd8c96eeab0a86ca5e11674e1f404d7fab
mazanma3 <mazanma3@fel.cvut.cz>
2012-02-29 20:32:21 +0100

    Annotation highlighting

--- cz.cvut.earlgrey.classmodel.ui/src/cz/cvut/earlgrey/classmodel/ui/syntaxcoloring/SemanticHighlightingCalculator.java
+++ cz.cvut.earlgrey.classmodel.ui/src/cz/cvut/earlgrey/classmodel/ui/syntaxcoloring/SemanticHighlightingCalculator.java
@@ -1,16 +1,16 @@
 package cz.cvut.earlgrey.classmodel.ui.syntaxcoloring;
 
-import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import cz.cvut.earlgrey.annotation.annotation.Annotation;
 
 /**
  * Use this class to register components to be used within the IDE.
  */
 public class SemanticHighlightingCalculator implements
-ISemanticHighlightingCalculator {
+		ISemanticHighlightingCalculator {
 
 	@Override
 	public void provideHighlightingFor(XtextResource resource,
@@ -19,68 +19,12 @@ ISemanticHighlightingCalculator {
 			return;
 		}
 
-		// Iterator<EObject> iter = EcoreUtil2.getAllContents(resource, true);
-		// while (iter.hasNext()) {
-		// EObject current = iter.next();
-		// if (current instanceof Annotation) {
-		// highlightFirstFeature(current,
-		// AnnotationPackage.Literals.ANNOTATION,
-		// HighlightingConfiguration.ANNOTATION_ID, acceptor);
-		// }
-		// }
-
 		INode root = resource.getParseResult().getRootNode();
 		for (INode node : root.getAsTreeIterable()) {
-			if (node.getGrammarElement() instanceof CrossReference) {
+			if (node.getSemanticElement() instanceof Annotation) {
 				acceptor.addPosition(node.getOffset(), node.getLength(),
-						HighlightingConfiguration.CROSSREFERENCE_ID);
+						HighlightingConfiguration.ANNOTATION_ID);
 			}
 		}
 	}
-
-	// // helper method that takes care of highlighting the first feature
-	// element
-	// // of a semantic object using a given text style ID
-	// private void highlightFirstFeature(EObject semobject, String featurename,
-	// String highlightID, IHighlightedPositionAcceptor acceptor) {
-	// // fetch the parse node for the entity
-	// ILeafNode nodetohighlight = getFirstFeatureNode(semobject, featurename);
-	// if (nodetohighlight != null) {
-	// acceptor.addPosition(nodetohighlight.getOffset(),
-	// nodetohighlight.getLength(), highlightID);
-	// }
-	// }
-	//
-	// /**
-	// * Taken from {@link http://blogs.itemis.de/stundzig/archives/467}.
-	// *
-	// * Adapted from Sebastian Zarnekow's semantic highlighting implementation
-	// * navigate to the parse node corresponding to the semantic object and
-	// fetch
-	// * the leaf node that corresponds to the first feature with the given
-	// name.
-	// *
-	// * @param semantic
-	// * @param feature
-	// * @return
-	// */
-	// public ILeafNode getFirstFeatureNode(EObject semantic, String feature) {
-	// ICompositeNode node = NodeModelUtils.getNode(semantic);
-	// if (node != null) {
-	// if (feature == null) {
-	// return null;
-	// }
-	// for (INode child : node.getChildren()) {
-	// if (child instanceof ILeafNode) {
-	// System.out.println(((ILeafNode) child).getText()
-	// + "         " + feature);
-	// if (feature.equals(((ILeafNode) child).getText())) {
-	// return (ILeafNode) child;
-	// }
-	// }
-	// }
-	// }
-	// return null;
-	// }
-
 }
